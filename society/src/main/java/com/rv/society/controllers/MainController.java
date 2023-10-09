e2596593822c24b68b3a30457f35a1fe17f3265b
Roman Stetsko <asgzxba@mail.ru>
2023-10-09 15:43:50 +0300

    After adding freemarker, all work now

--- society/src/main/java/com/rv/society/controllers/MainController.java
+++ society/src/main/java/com/rv/society/controllers/MainController.java
@@ -6,11 +6,11 @@ import com.rv.society.repos.MessageRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -26,13 +26,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-
-        //нашли все сообщения из репозитория
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
 
-        //положили их в модель, которую передаем в методе и отобразим на странице, "messages" совпадает с{{#messages}}
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
@@ -51,20 +56,20 @@ public class MainController {
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-
-        List<Message> messages = messageRepo.findByTag(filter);
-
-        if (messages.equals(null) || messages.isEmpty()) {
-            Iterable<Message> allMessages = messageRepo.findAll();
-            model.put("messages", allMessages);
-            return "main";
-        }
-        //Соответственно положит в модель в список сообщений только сообщения с одним тегом. Но тут с пути фильтра нет
-        model.put("messages", messages);
-        return "main";
-    }
+//    @PostMapping("filter")
+//    public String filter(@RequestParam String filter, Map<String, Object> model) {
+//
+//        List<Message> messages = messageRepo.findByTag(filter);
+//
+//        if (messages.equals(null) || messages.isEmpty()) {
+//            Iterable<Message> allMessages = messageRepo.findAll();
+//            model.put("messages", allMessages);
+//            return "main";
+//        }
+//        //Соответственно положит в модель в список сообщений только сообщения с одним тегом. Но тут с пути фильтра нет
+//        model.put("messages", messages);
+//        return "main";
+//    }
 
 
 }
